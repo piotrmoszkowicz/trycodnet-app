2667045e8f4fba947678e7929fe3205ba0e356e3
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-09-18 23:42:01 +0200

    Finish AddWallet page

--- src/views/AddWallet.jsx
+++ src/views/AddWallet.jsx
@@ -1,12 +1,40 @@
 import React from "react";
-import { withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
 
-const AddWalletPage = () => {
-  // TODO: Get Wallets
+import { addWallet } from "../api";
+
+import { message } from "antd";
+
+import WrappedAddWalletFrom from "../components/AddWalletForm";
+
+const AddWalletPage = (props) => {
+
+  const handlerSubmit = async e => {
+    e.preventDefault();
+    const walletAddress = e.target[0].value;
+
+    const data = {
+      walletAddress
+    };
+
+    try {
+      const response = await addWallet(data);
+      if (response.status !== 201) {
+        throw new Error();
+      }
+      message.success("Wallet has been added!");
+    } catch (e) {
+      message.error("Error while adding message");
+      console.error(e);
+    }
+
+    props.history.push("/");
+  };
 
   return (
-    <div>Add Wallet</div>
+    <WrappedAddWalletFrom handler={handlerSubmit}/>
   )
 };
+
 
 export default withRouter(AddWalletPage);
