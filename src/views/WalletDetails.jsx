29b3a75ba2cca137174804a5a4f319c695a6ad06
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-09-18 22:41:54 +0200

    Add basic views

---
+++ src/views/WalletDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+const WalletDetailsPage = () => {
+  // TODO: Get Wallets
+
+  return (
+    <div>Wallet details</div>
+  )
+};
+
+export default withRouter(WalletDetailsPage);
